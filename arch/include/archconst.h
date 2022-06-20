73c4af5fc45f6265bd662c624a689090ef82876a
zeim839 <michael@zeipekkis.com>
2022-06-20 15:52:06 -0400

    header for basic architecture constants

---
+++ arch/include/archconst.h
@@ -0,0 +1,17 @@
+#ifndef ARCH_CONST_H
+#define ARCH_CONST_H
+
+#define PIC_MSTR_CTR_PORT 0x20
+#define PIC_MSTR_DAT_PORT 0x21
+#define PIC_SLVE_CTR_PORT 0xA0
+#define PIC_SLVE_DAT_PORT 0xA1
+
+#define CLOCK_IRQ          32
+#define KEYBOARD_IRQ       33
+
+#define END_OF_INT        0x20
+
+#define IRQ0_VECTOR       0x20
+#define IRQ8_VECTOR       0x28
+
+#endif
