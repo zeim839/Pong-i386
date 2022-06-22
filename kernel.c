866422d51ef93c1c9a239ec455b0cf38062258a6
zeim839 <michael@zeipekkis.com>
2022-06-22 15:37:17 -0400

    updated to match new assets.h specs

--- kernel.c
+++ kernel.c
@@ -2,16 +2,33 @@
 #include "drivers/textmode.h"
 #include "drivers/clock.h"
 #include "drivers/pckbrd.h"
+#include "graphics/assets.h"
+
 extern void load_gdt();
 
-/* Temp. keyboard driver test. Will be replaced
-with main game loop in the future.*/
 void game(void)
 {
+  handle_ball();
+  draw_assets();
+
   unsigned char out = ' ';
   int i = kb_getchar(&out);
-
-  if (i) print(&out);
+  if (i) {
+    switch(out) {
+    case 'w':
+      mov_boardA(1);
+      break;
+    case 's':
+      mov_boardA(-1);
+      break;
+    case 'o':
+      mov_boardB(1);
+      break;
+    case 'k':
+      mov_boardB(-1);
+      break;
+    }
+  }
 }
 
 void sys_init(void)
@@ -19,18 +36,17 @@ void sys_init(void)
   load_gdt();
   init_interrupts();
 
-  init_clock(50);
+  init_clock(30);
   kb_init();
 
+  reset_assets();
   register_callback(game);
 }
 
 void kernel_entry()
 {
   sys_init();
-
   /* @TODO: Pong Game*/
 
   for(;;) asm("hlt");
 }
-
