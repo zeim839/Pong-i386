afc098f4e823454bfb97ac86d7117f3fd6a41fb2
zeim839 <michael@zeipekkis.com>
2022-06-27 19:19:56 +0400

    update clock speed dynamically based on ball movement

--- kernel.c
+++ kernel.c
@@ -5,30 +5,20 @@
 #include "graphics/assets.h"
 
 extern void load_gdt();
+uint32_t freq = 40;
+int direction = 0;
 
 void game(void)
 {
-  handle_ball();
-  draw_assets();
+  int _dir = handle_ball();
+  if (_dir != direction && _dir == 1)
+    init_clock(freq/1.5);
 
-  unsigned char out = ' ';
-  int i = kb_getchar(&out);
-  if (i) {
-    switch(out) {
-    case 'w':
-      mov_boardA(1);
-      break;
-    case 's':
-      mov_boardA(-1);
-      break;
-    case 'o':
-      mov_boardB(1);
-      break;
-    case 'k':
-      mov_boardB(-1);
-      break;
-    }
-  }
+  if (_dir != direction && _dir == 0)
+    init_clock(freq);
+
+  direction = _dir;
+  draw_assets();
 }
 
 void sys_init(void)
@@ -36,7 +26,7 @@ void sys_init(void)
   load_gdt();
   init_interrupts();
 
-  init_clock(30);
+  init_clock(freq);
   kb_init();
 
   reset_assets();
@@ -46,7 +36,5 @@ void sys_init(void)
 void kernel_entry()
 {
   sys_init();
-  /* @TODO: Pong Game*/
-
   for(;;) asm("hlt");
 }
